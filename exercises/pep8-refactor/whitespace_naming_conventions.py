9ab25281bb4037ec3dc18b72e304f6eae5f3dbbb
alaJream <jeremy.gonzales@students.dominican.edu>
2023-01-24 12:32:08 -0800

    init commit

--- exercises/pep8-refactor/whitespace_naming_conventions.py
+++ exercises/pep8-refactor/whitespace_naming_conventions.py
@@ -1,32 +1,32 @@
 # By Kami Bigdely
 # PEP8 - whitespaces and variable names.
 class pizza:
-    def __init__ (obj, mybread_type,CHEESE_TYPE,meatType,pizza_toppings,size):
-        obj.bread_type= mybread_type
-        obj.cheese_type = CHEESE_TYPE
-        obj.meatType= meatType
-        obj.toppings = pizza_toppings
-        obj.size = size        
+    def __init__ (self, my_bread_type, cheese_type, meat_type, pizza_toppings, size):
+        self.bread_type= my_bread_type
+        self.cheese_type = cheese_type
+        self.meat_type= meat_type
+        self.toppings = pizza_toppings
+        self.size = size        
     @classmethod
-    def Create_ChicagoPizza (class_type, size):
-        bread = 'deep-dish bread'
-        cheese = 'mozzarella cheese'
-        meatType= 'Italian sausage'
+    def createChicagoPizza (class_type, size):
+        bread_type = 'deep-dish bread'
+        cheese_type = 'mozzarella cheese'
+        meat_type = 'italian sausage'
         toppings = ['green bell pepper','mushroom', 'chunky tomato sauce', 'onion']
-        return class_type (bread, cheese, meatType, toppings, size)    
+        return class_type (bread_type, cheese_type, meat_type, toppings, size)    
     @classmethod
-    def createCalifornia_pizza(ct, meat_Type,size):
-        bread = 'thin crust'
-        CHEESE = 'feta cheese'
-        toppings =[ 'garlic', 'spinach', 'broccoli', 'olives', 'red onion', 'red bell pepper' ]
-        return ct(bread, CHEESE, meat_Type, toppings, size) 
+    def createCaliforniaPizza(class_type, meat_type, size):
+        bread_type = 'thin crust'
+        cheese_type = 'feta cheese'
+        toppings =['garlic', 'spinach', 'broccoli', 'olives', 'red onion', 'red bell pepper']
+        return class_type(bread_type, cheese_type, meat_type, toppings, size) 
     def printInfo(obj):
-        print('bread type is: ', obj.bread_type)
-        print('cheese type is: ', obj.cheese_type)
-        print('meat type is: ', obj.meatType)
+        print('Bread type is: ', obj.bread_type)
+        print('Cheese type is: ', obj.cheese_type)
+        print('Meat type is: ', obj.meat_type)
         print('Toppings are: ', end='')
         print(', '.join(map(str, obj.toppings)))
 
     
-myPizza = pizza.createCalifornia_pizza('chicken', 'large')
+myPizza = pizza.createCaliforniaPizza('chicken', 'large')
 myPizza.printInfo()
