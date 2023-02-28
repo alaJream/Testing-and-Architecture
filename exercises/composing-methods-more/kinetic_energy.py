45a58612e8666e7ce936c35fbe5dfb9ea7b9f36a
alaJream <jeremy.gonzales@students.dominican.edu>
2023-02-28 11:49:59 -0800

    methods and more

--- exercises/composing-methods-more/kinetic_energy.py
+++ exercises/composing-methods-more/kinetic_energy.py
@@ -1,6 +1,3 @@
-# By Kami Bigdely
-# Remove assignment to method parameter.
-
 class Distance:
     def __init__(self, value, unit):
         self.unit = unit
@@ -11,31 +8,41 @@ class Mass:
         self.value = value
         self.unit = unit
 
-def calculate_kinetic_energy(mass, distance, time):
-    if distance.unit != 'km':
-        if distance.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
-            # convert from light-year to km unit
-            in_km = distance.value * 9.461e12
-            distance = Distance(in_km, "km")
-        else:
-            print ("unit is Unknown")
-            return
-    speed = distance.value/time # [km per sec]
-    if mass.unit != 'kg':
-        if mass.unit == "solar-mass":
-            # convert from solar mass to kg
-            value = mass.value * 1.98892e30 # [kg]
-            mass = Mass(value, 'kg')
-        else:
-            print ("unit is Unknown")
-            return
+def convert_distance_to_km(distance):
+    if distance.unit == 'km':
+        return distance.value
+    elif distance.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
+        # convert from light-year to km unit
+        in_km = distance.value * 9.461e12
+        return in_km
+    else:
+        print("distance unit is unknown")
+        return None
 
-    kinetic_energy = 0.5 * mass.value * speed ** 2
+def convert_mass_to_kg(mass):
+    if mass.unit == 'kg':
+        return mass.value
+    elif mass.unit == "solar-mass":
+        # convert from solar mass to kg
+        value = mass.value * 1.98892e30 # [kg]
+        return value
+    else:
+        print("mass unit is unknown")
+        return None
+
+def calculate_kinetic_energy(mass, distance, time):
+    distance_in_km = convert_distance_to_km(distance)
+    if distance_in_km is None:
+        return None
+
+    mass_in_kg = convert_mass_to_kg(mass)
+    if mass_in_kg is None:
+        return None
+
+    speed = distance_in_km / time # [km per sec]
+    kinetic_energy = 0.5 * mass_in_kg * speed ** 2
     return kinetic_energy
 
 mass = Mass(2, "solar-mass")
 distance = Distance(2, 'ly')
-print(calculate_kinetic_energy(Mass(mass.value, mass.unit), Distance(distance.value, distance.unit), 3600e20))
-
-
-
+print(calculate_kinetic_energy(mass, distance, 3600e20))
--- exercises/composing-methods-more/save_to_db.py
+++ exercises/composing-methods-more/save_to_db.py
@@ -8,7 +8,7 @@ username = input('Please enter your username: ')
 save_into_db(username)
 
 birth_year_input = int(input('Please enter your birth year: '))
-current_year = 2020
+current_year = 2023
 age = current_year - birth_year_input
 print("You are", age, "years old.")
 
