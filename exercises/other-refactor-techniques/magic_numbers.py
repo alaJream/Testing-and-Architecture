69278fc943405aac68adc1f1342f226f8fddae5f
alaJream <jeremy.gonzales@students.dominican.edu>
2023-02-28 12:02:12 -0800

    dropped questions other refactor techniques

--- exercises/other-refactor-techniques/magic_numbers.py
+++ exercises/other-refactor-techniques/magic_numbers.py
@@ -1,16 +1,22 @@
 # by Kami Bigdely 
-# Replace magic numbers with named constanst
+# Replace magic numbers with named constants
+
+CONSTANT = 8.9875517923 * 1e9
 
 # First Section
-# Given two point charges, calcualte the electric force exerted on them.
+# Given two point charges, calculate the electric force exerted on them.
 q1 = int(input('Enter a value of charge q1: '))
 q2 = int(input('Enter a value of charge q2: '))
-distance = int(input("Enter the distance be10tween two charges: "))
-force = 8.9875517923*1e9 * q1 * q2/(distance**2)
-print ("Electric Force between q1 and q2 is: ", force, "Newton")
-# Second Section
+distance = int(input("Enter the distance between two charges: "))
+force = CONSTANT * q1 * q2 / (distance ** 2)
+print("Electric Force between q1 and q2 is: ", force, "Newton")
+
+# Second Section, define constants
+EVEN = "even"
+ODD = "odd"
+
 num = int(input('Enter an integer number: '))
 if num % 2 == 0:
-    print(num, "is an even number.")
+    print(num, "is an", EVEN, "number.")
 else:
-    print(num, "is an odd number.")
+    print(num, "is an", ODD, "number.")
